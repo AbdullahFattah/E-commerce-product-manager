b3151956911897520fdf0a6f0d4e3a0e538f9d5c
Abdullah <abdullah.fattah808@gmail.com>
2022-08-04 03:33:46 +0200

    Basic form added

--- src/components/add-product.js
+++ src/components/add-product.js
@@ -11,6 +11,30 @@ function addProduct(){
             Cancel
           </Link>
         </div>
+        <form className="container col-md-4 my-5" id="product_form">
+  <div className="form-group">
+    <label>SKU</label>
+    <input type="text" className="form-control" id="product_sku" placeholder="Enter SKU"></input>
+  </div>
+  <div className="form-group">
+    <label>Name</label>
+    <input type="text" className="form-control" id="product_name" placeholder="Enter product name"></input>
+  </div>
+  <div className="form-group">
+    <label>Price</label>
+    <input type="text" className="form-control" id="product_price" placeholder="Enter product price"></input>
+  </div>
+  <div className="form-group">
+    <label>Type</label>
+    <select className="form-control" id="productType">
+      <option>Select product type</option>
+      <option>Type 1</option>
+      <option>Type 2</option>
+      <option>Type 3</option>
+    </select>
+  </div>
+  
+</form>
         </>
     )
 }
